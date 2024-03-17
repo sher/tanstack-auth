2217b57b5120174e9f75d6af38060082b3d92430
sher <sherzod@gmail.com>
2024-03-17 22:59:30 +0900

    add app logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,115 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { createRoute, createRouter, createRootRouteWithContext, RouterProvider, redirect } from '@tanstack/react-router';
+import { useQuery, QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { createContext, useContext, type PropsWithChildren } from 'react';
 
-function App() {
-  const [count, setCount] = useState(0)
+//-------------------------------------
+//- Auth
+//-------------------------------------
+type User = {
+  user_id: number;
+  username: string;
+};
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+type AuthState = {
+  user: User | undefined;
+  isAuthRequestFinished: boolean;
+};
+
+const initialState = { user: undefined } as AuthState;
+const AuthContext = createContext(initialState);
+
+function AuthProvider({ children }: PropsWithChildren) {
+  const { isFetched: isAuthRequestFinished, data: user } = useQuery<User>({
+    queryKey: ['auth'],
+    queryFn: async function () {
+      // Simulate a slow network request
+      await new Promise((resolve) => setTimeout(resolve, 2000));
+      const data = localStorage.getItem('user');
+      const user = data ? JSON.parse(data) : null;
+      return user;
+    },
+    staleTime: 0,
+    retry: false,
+  });
+
+  console.log('AuthProvider', { user, isAuthRequestFinished });
+
+  return <AuthContext.Provider value={{ user, isAuthRequestFinished }}>{children}</AuthContext.Provider>;
 }
 
-export default App
+function useAuth() {
+  const context = useContext(AuthContext);
+  if (context === initialState) {
+    throw new Error('useAuth must be used within a AuthProvider');
+  }
+  return context;
+}
+
+//-------------------------------------
+//- Router
+//-------------------------------------
+type RootContext = {
+  queryClient: QueryClient;
+  auth: AuthState;
+};
+
+const rootRoute = createRootRouteWithContext<RootContext>()({
+  beforeLoad: function ({ context }) {
+    const { isAuthRequestFinished, user } = context.auth;
+    // If we made a request and couldn't find a user, redirect to the login page
+    console.log('beforeLoad', { isAuthRequestFinished, user });
+    if (isAuthRequestFinished && !user) {
+      throw redirect({ to: '/login' });
+    }
+  },
+});
+
+const homeRoute = createRoute({
+  path: '/',
+  component: () => <h1>Home</h1>,
+  getParentRoute: () => rootRoute,
+});
+
+const loginRoute = createRoute({
+  path: '/login',
+  component: () => <h1>Login</h1>,
+  getParentRoute: () => rootRoute,
+});
+
+const routeTree = rootRoute.addChildren([homeRoute, loginRoute]);
+
+const queryClient = new QueryClient();
+
+const router = createRouter({
+  routeTree,
+  context: {
+    queryClient,
+    auth: { user: undefined, isAuthRequestFinished: false },
+  },
+});
+
+declare module '@tanstack/react-router' {
+  interface Register {
+    router: typeof router;
+  }
+}
+
+//-------------------------------------
+//- App
+//-------------------------------------
+function InnerApp() {
+  const { user, isAuthRequestFinished } = useAuth();
+  return <RouterProvider router={router} context={{ queryClient, auth: { user, isAuthRequestFinished } }} />;
+}
+
+function App() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <AuthProvider>
+        <InnerApp />
+      </AuthProvider>
+    </QueryClientProvider>
+  );
+}
+
+export default App;
